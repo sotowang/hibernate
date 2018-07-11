8161ed771b873ae8eea6a2c8eed5861e705917d2
sotowang <sotowang@qq.com>
2018-07-11 14:47:36 +0800

    更新:   多对多   一对多   多对一

--- src/com/soto/test/TsetHibernate.java
+++ src/com/soto/test/TsetHibernate.java
@@ -1,10 +1,15 @@
 package com.soto.test;
 
 
+import com.soto.pojo.Category;
 import com.soto.pojo.Product;
+import com.soto.pojo.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class TsetHibernate {
     public static void main(String[] args) {
@@ -16,6 +21,7 @@ public class TsetHibernate {
         s.beginTransaction();
 
 //        4. 通过调用Session的save方法把对象保存到数据库
+//
 //        for (int i = 0; i < 10; i++) {
 //            Product p = new Product();
 //            p.setName("iphone" + i);
@@ -24,11 +30,48 @@ public class TsetHibernate {
 //
 //        }
 
-        Product p = (Product)s.get(Product.class, 3);
-        System.out.println("id=6的产品名称是: " + p.getName());
+//        Product p = (Product)s.get(Product.class, 3);
+//        System.out.println("id=6的产品名称是: " + p.getName());
+//
+//        p.setName("iphonr-modified");
+//        s.update(p);
+        /**
+         * 测试many-to-one关系
+         */
+//        Category c = new Category();
+//        c.setName("c1");
+//        s.save(c);
+//
+//        Product p = (Product) s.get(Product.class, 8);
+//        p.setCategory(c);
+//        s.update(p);
 
-        p.setName("iphonr-modified");
-        s.update(p);
+        /**
+         * 测试one-to-many关系
+         */
+//        Category c = (Category) s.get(Category.class, 1);
+//        Set<Product> ps = c.getProducts();
+//        for (Product p : ps) {
+//            System.out.println(p.getName());
+//        }
+        /**
+         * 测试may-to-many
+         */
+        //增加3个用户
+        Set<User> users = new HashSet();
+        for (int i = 0; i < 3; i++) {
+            User u =new User();
+            u.setName("user"+i);
+            users.add(u);
+            s.save(u);
+        }
+        Product p1 = (Product) s.get(Product.class, 5);
+
+        p1.setUsers(users);
+        s.save(p1);
+
+
+
 //        s.delete(p);
 
 //        5. 提交事务
